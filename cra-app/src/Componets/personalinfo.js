40281e87ffcca096be4373f9682e2e0f4a67f274
iamkrincy <krincy6819@gmail.com>
2025-01-10 11:15:50 +0530

    k

---
+++ cra-app/src/Componets/personalinfo.js
@@ -0,0 +1,16 @@
+
+
+
+const PersonalInfo = () => {
+  return (
+    <div>
+        <h2>Personal Information</h2>
+        <h4>Name :- Savaliya Krincy </h4>
+        <h4>Age :- 18 </h4>
+        <h4>Address :- B-303,siddheshwar heights,valak </h4>
+        <h4>City :- Surat,Gujarat</h4>
+    </div>
+  )
+};
+
+export default  PersonalInfo;
